8f0b1ef412c8a436a30d789aa844140ed45d8c87
Abdulloh Xo'jayev <xodjaevabdulloh54@gmail.com>
2024-04-26 11:46:50 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,13 @@
+let abdulloh = ["Abdulloh", 11, true, "qora qamish"];
+ console.log(abdulloh);
+ console.log(abdulloh.length);
+
+ let rost = confirm("Manzilingiz'togrimi");
+
+ if (rost) {
+
+ }else{
+    abdulloh.pop();
+ }
+
+ console.log(abdulloh);
